34f914ed83d1d7fb69ee831a29b86def386f204a
Matthew Bokovitz <MattBokovitz@Matthews-MBP-2.fios-router.home>
2021-01-20 18:06:23 -0500

    Finished MVP

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,40 @@
-// scheme-model
+const db = require("../../data/db-config");
+
+module.exports = {
+  find() {
+    return db("schemes");
+  },
+
+  findById(id) {
+    return db("schemes").where("id", id).first();
+  },
+
+  findSteps(id) {
+    return db("steps")
+      .join("schemes", "steps.scheme_id", "schemes.id")
+      .select(
+        "steps.id",
+        "schemes.scheme_name",
+        "steps.step_number",
+        "steps.instructions"
+      )
+      .where("schemes.id", id)
+      .orderBy("steps.step_number");
+  },
+
+  add(scheme) {
+    return db("schemes")
+      .insert(scheme)
+      .then(([id]) => {
+        return db("schemes").where("id", id).first();
+      });
+  },
+
+  update(changes, id) {
+    return db("schemes").where("id", id).update(changes);
+  },
+
+  remove(id) {
+    return db("schemes").where("id", id).del();
+  },
+};
